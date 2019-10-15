52451d17d6b008459f04013e25b98da89dae1b0b
Chase <PengXu1993@gmail.com>
2019-10-14 23:06:20 -0700

    js, boostrap, css, less

---
+++ Pages/index.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $("#title").hover(function(){
+//         document.getElementById("title").innerHTML = "Hello JavaScript";
+//     },
+//     function(){
+//       alert("Bye! You now leave p1!");
+//     }); 
+//   });
