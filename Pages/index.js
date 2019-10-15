23c9364c2910d94cbcceceadfc1f80f07783f69e
Chase-surface <pengxu1993@gmail.com>
2019-10-15 10:10:08 -0700

    react project
    created react projects

--- Pages/index.js
+++ Pages/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // $(document).ready(function(){
 //     $("#title").hover(function(){
 //         document.getElementById("title").innerHTML = "Hello JavaScript";
@@ -6,3 +7,22 @@
 //       alert("Bye! You now leave p1!");
 //     }); 
 //   });
+=======
+// class Car extends React.Component {
+//     render() {
+//       return <h2>I am a Car!</h2>;
+//     }
+//   }
+  
+//   class Garage extends React.Component {
+//     render() {
+//       return (
+//         <div>
+//         <h1>Who lives in my Garage?</h1>
+//         <Car />
+//         </div>
+//       );
+//     }
+//   }
+//   ReactDOM.render(<Garage />, document.getElementById('root'));
+>>>>>>> Stashed changes
---
+++ tutorme/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header class="navbar navbar-light  navbar-expand flex-column flex-md-row bg-light">
+      <a class="navbar-brand " id="title" href="index.html">
+          <h2>TutorMe</h2>
+      </a>
+      <div id="nav">
+          
+      </div>
+    </header>
+  );
+}
+
+export default App;
