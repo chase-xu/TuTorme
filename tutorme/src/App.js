d70519d898f21ad0d09b97d26e2748109a174958
Chase-surface <pengxu1993@gmail.com>
2019-10-16 18:53:27 -0700

    nar bar finished

--- tutorme/src/App.js
+++ tutorme/src/App.js
@@ -4,18 +4,26 @@ import './App.css';
 
 
 
-class nav extends React.Component{
+class Menu extends React.Component{
   render(){
     return(
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
           </li>
-          <li>About Us</li>
-          <li>FAQS</li>
-          <li>BeTutor</li>
-          <li>Sign In</li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FAQS<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">BeTutor<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign In<span class="sr-only">(current)</span></a>
+          </li>
         </ul>
       </div>
     );
@@ -23,12 +31,13 @@ class nav extends React.Component{
 }
 
 class App extends React.Component {
- render(){ return (
+ render(){ 
+   return (
     <header class="navbar navbar-light  navbar-expand flex-column flex-md-row bg-light">
       <a class="navbar-brand " id="title" href="index.html">
           <h2>TutorMe</h2>
       </a>
-      <nav />
+      <Menu />
     </header>
   );
  }
