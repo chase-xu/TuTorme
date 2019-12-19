2b79dc896fd0b217b33102ca07c65096431e895e
Chase-surface <pengxu1993@gmail.com>
2019-12-18 17:50:18 -0800

    thunk

---
+++ tutorme/src/actions/projectActions.js
@@ -0,0 +1,7 @@
+export const createProject = (project) =>{
+    return (dispatch, getState)=>{
+        //make async data call to database
+        dispatch({type:'CREATE_PROJECT', project});
+    }
+
+};
