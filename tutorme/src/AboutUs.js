464ab6915d66a2499848d548d99f5ad764230e36
Chase <PengXu1993@gmail.com>
2020-01-18 23:56:42 -0800

    Update AboutUs.js

--- tutorme/src/AboutUs.js
+++ tutorme/src/AboutUs.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 class AboutUs extends React.Component{
     render(){
         return(
-            <div>
+            <div class="bg-light mt-3 mr-4 ml-4 p-5">
                 <h2>About Us</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus ullamcorper placerat. Nunc congue ante vitae nulla venenatis, sit amet ornare tellus faucibus. Vestibulum erat nibh, hendrerit sit amet leo sit amet, tincidunt consequat risus. Phasellus elit dui, pellentesque nec rhoncus nec, luctus gravida felis. Sed ut ligula eu quam tempus bibendum quis vel lorem. Cras feugiat nec lorem at ullamcorper. Donec rutrum, purus non sollicitudin tempus, nibh leo porta lorem, a tristique velit risus sit amet arcu. Pellentesque non nisl id nisi iaculis interdum in ut lectus. Fusce eleifend quis dui sit amet sodales. Mauris felis nulla, consectetur ut aliquet a, vehicula et nisi.
